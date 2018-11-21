df2248812408b6bd53915ce69f0dc9f584a942cb
LJH <1430423309@qq.com>
2018-11-21 13:06:31 +0800

    add view

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
+from app import app
 
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-
-admin = Admin(app, template_mode='bootstrap3')
-
-from app import views, models
+if __name__ == '__main__':
+    app.run(debug=True)
